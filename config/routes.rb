0b501892e462765cc2b458fc60b2203bb13e4754
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-23 14:05:39 -0500

    commit tree c429ad974aa761f0fd519a180b8b07cfa4d1e901

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+
+  patch 'artists/:id', to: 'artists#update' 
+  patch 'genres/:id', to: 'genres#update' 
+  patch 'songs/:id', to: 'songs#update' 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
